70b2f0471681f66c051154a4fcdf3ebac8a86b4b
Victor Wei <vywei@Victors-MacBook.local>
2018-02-26 14:45:42 -0800

    first commit

---
+++ src/main/java/WeightConversion.java
@@ -0,0 +1,12 @@
+public class WeightConversion {
+
+    public double KilosToPounds (double kilos){
+        
+        return (kilos / 0.454);
+    }
+    
+public double PoundsToKilos (double pounds){
+        
+        return (pounds * 0.454);
+    }
+}
